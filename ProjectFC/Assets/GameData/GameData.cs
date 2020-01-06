020c3bf66bf3ab0e0da9a0afd32e594ae23203d2
PlayingSpree <winingame@hotmail.com>
2020-01-06 19:47:08 +0700

    FlyingCapsule
    Prototype flying

---
+++ ProjectFC/Assets/GameData/GameData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class GameData
+{
+	public class Suit
+	{
+		public float dragMin = 1f;
+		public float dragMax = 3f;
+		public float dragAngleMin = 1f;
+		public float dragAngleMax = 3f;
+		public float weight = 50f;
+	}
+	public class GravShoe
+	{
+		public float power = 1200f;
+		public float control = 3f;
+	}
+}
---
+++ ProjectFC/Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Ref
+    new Rigidbody rigidbody;
+    // Status
+    public Vector3 currentForce;
+    public Vector3 targetForce;
+    public float currentDrag;
+    public float targetDrag;
+    // Enquipment
+    public GameData.GravShoe shoe = new GameData.GravShoe();
+    public GameData.Suit suit = new GameData.Suit();
+
+    void Awake()
+    {
+        // Set Ref
+        rigidbody = GetComponent<Rigidbody>();
+
+        // Init stat
+        rigidbody.mass = suit.weight;
+        currentDrag = suit.dragMax;
+        targetDrag = suit.dragMax;
+
+        // Floating Still
+        currentForce = rigidbody.mass * -Physics.gravity;
+        targetForce = rigidbody.mass * -Physics.gravity;
+    }
+
+    void FixedUpdate()
+    {
+        // Drag
+        currentDrag = Mathf.Lerp(currentDrag, targetDrag, Time.fixedDeltaTime * Mathf.Max((200f - suit.weight) / 100f, 1f));
+        // Floating
+
+
+        // Calulate Force
+        currentForce = Vector3.Lerp(currentForce, Quaternion.Inverse(transform.rotation) * targetForce, Time.fixedDeltaTime * shoe.control);
+        // Add Force
+        rigidbody.AddRelativeForce(currentForce, ForceMode.Force);
+
+        Vector3 worldForce = transform.rotation * currentForce;
+        Debug.DrawLine(transform.position, transform.position + worldForce);
+    }
+}
